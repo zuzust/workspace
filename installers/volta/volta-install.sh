99a4204ad5a445cbda2822f09173497efa7abcb3
Mr Windmills <mrwindmills.dev@gmail.com>
2021-08-11 19:47:03 +0200

    volta: show post-install message to install dev toolchain

--- installers/volta/volta-install.sh
+++ installers/volta/volta-install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-VOLTA_INSTALLER_DIR=$WORKSPACE_DIR/installers/volta
-VOLTA_DIR=$HOME/.volta
+VOLTA_INSTALLER_DIR="${WORKSPACE_DIR}/installers/volta"
+VOLTA_HOME="${HOME}/.volta"
 
 # Install volta (javascript tools manager)
 curl https://get.volta.sh | bash -s -- --skip-setup
 
 # Install completions
-$VOLTA_DIR/bin/volta completions -f -o $ZSH_DOTFILES_DIR/completion.d/_volta zsh
+$VOLTA_HOME/bin/volta completions -f -o $ZSH_DOTFILES_DIR/completion.d/_volta zsh
 
 # Setup env
 ln -s $VOLTA_INSTALLER_DIR/volta.zenv $ZSH_DOTFILES_DIR/env.d/30_volta.zenv
---
+++ installers/volta/volta-toolchain-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Install toolchain
+volta install \
+  node@14.15.4 \
+  npm@7.18.1 \
+  yarn@1.22.10 \
+  arweave-deploy@1.9.1 \
+  backstopjs@5.3.7 \
+  bit-bin@14.8.8 \
+  ghost-cli@1.17.3
+
+
+# vim:ft=sh
+# File: $WORKSPACE_DIR/installers/volta/volta-toolchain-install.sh
